ef905d8a18ed312b98c71bd5aab9701872fa66c8
Young Kim <young-k@protonmail.com>
2017-10-25 22:11:11 -0400

    modified manifest; fixed rsync script

--- script.sh
+++ script.sh
@@ -1,1 +1,1 @@
-rsync ./sketch-to-swift.sketchplugin ~/Library/Application\ Support/com.bohemiancoding.sketch3/Plugins
+rsync -avv ./sketch-to-swift.sketchplugin ~/Library/Application\ Support/com.bohemiancoding.sketch3/Plugins/sketch-to-swift.sketchplugin
