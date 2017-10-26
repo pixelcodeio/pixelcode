be2fee5cacdf258992837c72b39e7cd48a6cff49
Young Kim <young-k@protonmail.com>
2017-10-25 23:20:00 -0400

    init interpreter & parser

---
+++ app/interpreter.py
@@ -0,0 +1,6 @@
+"""
+Takes output from Parser one at a time and generates swift file
+"""
+class Interpreter:
+  def __init__(self):
+    pass
---
+++ app/parser.py
@@ -0,0 +1,9 @@
+"""
+Parses a SVG file and outputs a dictionary with necessary attributes
+"""
+class Parser:
+  """
+  Returns: Parser object for parsing the file located at filepath
+  """
+  def __init__(self, filepath):
+    pass
