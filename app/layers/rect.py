52e8d7779b2c932b6d2882ecd0a2672bdce94810
Young Kim <young-k@protonmail.com>
2017-11-04 00:15:34 -0400

    added docstring for rect function

--- app/layers/rect.py
+++ app/layers/rect.py
@@ -11,6 +11,19 @@ class Rect(object):
     self.elem = self.parse_rect(elem, vertical, horizontal)
 
   def parse_rect(self, elem, vertical, horizontal):
+    """
+    Args:
+      Refer to args in __init__
+      
+    Returns: dictionary with keys
+      - type (str): "UIView"
+      - id (str): id of the element
+      - fill (tuple): (r, g, b) triple representing the color
+      - x (float): x coordinate with respect to global width
+      - y (float): y coordinate with respect to global height
+      - vertical (dict): refer to __init__ args for description
+      - horizontal (dict): refer to __init__ args for description
+    """
     center_x = ((int(elem["x"]) + elem["width"]) / 2)
     center_y = ((int(elem["y"]) + elem["height"]) / 2)
     return {
