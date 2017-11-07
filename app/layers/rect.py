5d09ff94d4029298b492f5349f7e96f0fc6e7348
Young Kim <young-k@protonmail.com>
2017-11-07 03:41:30 -0500

    rectangle correctly done in parser

--- app/layers/rect.py
+++ app/layers/rect.py
@@ -8,4 +8,16 @@ class Rect(BaseLayer):
   def parse_elem(self, elem):
     if elem["fill"] != "none":
       elem["fill"] = utils.convert_hex_to_rgb(elem["fill"])
+    if "rx" in elem.attrs:
+      elem["border-radius"] = elem["rx"]
+    if "stroke" in elem.attrs:
+      elem["stroke-color"] = utils.convert_hex_to_rgb(elem["stroke"])
+
+      if "stroke-width" in elem:
+        elem["stroke-width"] = elem["stroke-width"]
+      else:
+        elem["stroke-width"] = 1
+    else:
+      elem["stroke-color"] = None
+      elem["stroke-width"] = None
     return super(Rect, self).parse_elem(elem)
