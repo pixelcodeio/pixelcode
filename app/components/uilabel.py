03ee55e7ee70e1d5e7ec762bc6f0070f799a3f94
Kevin Chan <kevinchan159@gmail.com>
2017-11-08 01:27:13 -0500

    cleaned up code into base_components

--- app/components/uilabel.py
+++ app/components/uilabel.py
@@ -1,17 +1,12 @@
 import components.utils as utils
 
 class UILabel(object):
-  def __init__(self, info, bgColor):
+  def __init__(self, bgColor):
     """
     Args:
-      info: Refer to generate_label for documentation of info.
       bgColor: (tuple) Background color of label as r, g, b values
-
-    Returns: UILabel object with the necessary swift code
     """
     self.bgColor = bgColor
-    self.swift = self.generate_label(info)
-    return
 
   def set_text(self, elem, txt):
     """
@@ -69,74 +64,3 @@ class UILabel(object):
     """
     return ("{}.font = UIFont(name: \"{}\", size: {})\n"
            ).format(elem, font, size)
-
-  def generate_label(self, info):
-    """
-    Args:
-      info: is a dictionary of keys:
-        - id: (str) name of view
-        - text: (str) text that is to be displayed
-        - text-color: (tuple) r, g, b values of the text color
-        - font-size: (int) font-size of the text
-        - font-weight: (optional int) font weight of title. Has value None if
-                       no value is provided
-        - font-family: (str) name of the font of title
-        - x: (float) x-coor of view's center as percentage of screen's width
-        - y: (float) y-coor of view's center as percentage of screen's height
-        - vertical: (dict) dict containing constraints for top/bottom of view
-        - horizontal: (dict) dict containing constraints for left/right of view
-        - width: (float) width of view as percentage of screen's width
-        - height: (float) height of view as percentage of screen's height
-        - stroke-color: (optional tuple) r, g, b values representing the border
-                        color. Has value None if no value is provided
-        - stroke-width: (optional int) the number of pixels representing the
-                        border width. Has value None if no value is provided
-        - border-radius: (optional int) the number of pixels representing the
-                         corner radius. Has value None if no value is provided
-
-    Returns: The swift code to generate a label with a text
-    """
-    vertical = info['vertical']
-    horizontal = info['horizontal']
-    verticalDir = vertical['direction']
-    verticalID = vertical['id']
-    verticalDist = vertical['distance']
-    horizontalDir = horizontal['direction']
-    horizontalID = horizontal['id']
-    horizontalDist = horizontal['distance']
-    centerX = info['x']
-    centerY = info['y']
-    width = info['width']
-    height = info['height']
-    lid = info['id']
-    txt = info['text']
-    txtAlign = info['text-align']
-    txtColor = info['text-color']
-    fontSize = info['font-size']
-    fontW = info['font-weight']
-    fontFamily = info['font-family']
-    borColor = info['stroke-color']
-    borWidth = info['stroke-width']
-    corRad = info['border-radius']
-    label = 'var {} = UILabel()\n'.format(lid)
-    label += utils.translates_false(lid)
-    label += self.set_text(lid, txt)
-    label += self.set_text_color(lid, txtColor)
-    label += self.set_bg_color(lid)
-    if fontW != None:
-      label += self.set_font_size_weight(lid, fontSize, fontW)
-    else:
-      label += self.set_font_size(lid, fontSize)
-    if txtAlign is None:
-      txtAlign = "center"
-    label += self.set_font_family(lid, fontFamily, fontSize)
-    label += utils.set_border_color(lid, borColor) if borColor != None else ""
-    label += utils.set_border_width(lid, borWidth) if borWidth != None else ""
-    label += utils.set_corner_radius(lid, corRad) if corRad != None else ""
-    label += self.center_and_wrap(lid, txtAlign)
-    label += utils.add_subview('view', lid)
-    label += utils.wh_constraints(lid, width, height)
-    label += utils.position_constraints(
-        lid, horizontalID, horizontalDir, horizontalDist, verticalID,
-        verticalDir, verticalDist, centerX, centerY)
-    return label
