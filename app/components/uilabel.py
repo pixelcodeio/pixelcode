0ab945638d48a498db1306fa0ebf70f9e44e830c
Kevin Chan <kevinchan159@gmail.com>
2017-11-10 23:34:05 -0500

    updated parser

--- app/components/uilabel.py
+++ app/components/uilabel.py
@@ -8,11 +8,32 @@ class UILabel(object):
     """
     self.bgColor = bgColor
 
+  def create_attributed_str(self, elem, text):
+    """
+    Args:
+      elem: (str) id of element
+      text: (str) text of the attributed string.
+
+    Returns: The swift code to create an attributed string.
+    """
+    return ("{}AttributedStr = NSMutableAttributedString(string: {})\n"
+           ).format(elem, text)
+
   def set_text(self, elem, txt):
     """
     Returns: The swift code to set the text of elem to be txt
     """
     return '{}.text = "{}"\n'.format(elem, txt)
+
+  def set_attributed_text(self, elem, strID):
+    """
+    Args:
+      elem: (str) id of element
+      strID: (str) id of attributed string
+
+    Returns: The swift code to set the attributedText property of elem.
+    """
+    return ("{}.attributedText = {}\n").format(elem, strID)
 
   def set_text_color(self, elem, color):
     """
@@ -70,3 +91,40 @@ class UILabel(object):
     Returns: The swift code to set numberOfLines to be 0 for elem.
     """
     return "{}.numberOfLines = 0\n".format(elem)
+
+  def set_substring_color(self, strID, r, g, b, start, length):
+    """
+    Args:
+      strID: (string) the variable name of string that is to be edited
+      r: (int) red rgb value
+      g: (int) green rgb value
+      b: (int) blue rgb value
+      start: (int) index of first character whose color is being changed
+      length: (int) number of characters from start index whose color is being
+              changed.
+
+    Returns: The swift code to set a substring of str to be a color with r,g,b
+             values.
+    """
+    color = ("UIColor(red: {}/255.0, green: {}/255.0, blue: {}/255.0, alpha)"
+             ": 1.0)"
+            ).format(r, g, b)
+    return ("{}.addAttribute(NSForegroundColorAttributeName, value: {})"
+            ", range: NSRange(location: {}, length: {}))\n"
+           ).format(strID, color, start, length)
+
+  def set_substring_font(self, strID, font, size, start, length):
+    """
+    Args:
+      strID: (string) the variable name of string that is to be edited
+      font: (string) the font of the substring
+      start: (int) index of first character whose font is being changed
+      length: (int) number of characters from start index whose font is being
+              changed.
+
+    Returns: The swift code to set a substring of str to be a font.
+    """
+    font = ('UIFont(name: "{}", size: {})').format(font, size)
+    return ("{}.addAttribute(NSFontAttributeName, value: {}"
+            ", range: NSRange(location: {}, length: {}))\n"
+           ).format(strID, color, start, length)
--- app/components/uitextfield.py
+++ app/components/uitextfield.py
@@ -7,8 +7,13 @@ class UITextField(object):
   def __init__(self):
     pass
 
-  def set_placeholder(self, tid, text):
-    return '{}.placeholder = "{}"'.format(tid, text)
+  def set_placeholder_text_and_color(self, tid, text, r, g, b):
+    color = ("UIColor(red: {}/255.0, green: {}/255.0, blue: {}/255.0, alpha"
+             ": 1.0)"
+            ).format(r, g, b)
+    return ('{}.attributedPlaceholder = NSAttributedString(string: "{}", '
+            'attributes: [NSAttributedStringKey.foregroundColor: {}])\n'
+           ).format(tid, text, color)
 
   def set_left_inset(self, tid, left):
     return ('{}.layer.sublayerTransform = CATransform3DMakeTranslations({}'
