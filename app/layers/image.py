a9124630b717f0ac6d50924ad91336c9f1448876
Young Kim <young-k@protonmail.com>
2017-11-13 00:07:14 -0500

    pushing updated path

--- app/layers/image.py
+++ app/layers/image.py
@@ -5,5 +5,5 @@ class Image(BaseLayer):
   Class representing an Image layer in Sketch
   """
   def parse_elem(self, elem):
-    elem["path"] = elem["id"]
+    elem["path"] = elem["id"] + ".png" # TODO: change this later
     return super(Image, self).parse_elem(elem)
