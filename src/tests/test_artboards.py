d2b08c7896ee78f490fad1c8b69c8da57b1f206b
Young Kim <young-k@protonmail.com>
2017-11-16 00:26:45 -0500

    rewrote tests

--- src/tests/test_artboards.py
+++ src/tests/test_artboards.py
@@ -3,25 +3,25 @@ import unittest
 class TestStringMethods(unittest.TestCase):
 
   def filename_helper(self, filename):
-    f1 = open("./tests/" + filename, "r+")
-    with open("./app/tests/" + filename, "r+") as f2:
+    f1 = open("./src/tests/" + filename + ".out", "r+")
+    with open("./src/tests/" + filename + ".correct", "r+") as f2:
       for line in f2:
         self.assertEqual(f1.readline(), line)
 
   def test_images(self):
-    self.filename_helper("images.out")
+    self.filename_helper("images")
 
   def test_text(self):
-    self.filename_helper("text.out")
+    self.filename_helper("text")
 
   def test_rect_borders(self):
-    self.filename_helper("rectBorders.out")
+    self.filename_helper("rectBorders")
 
   def test_input(self):
-    self.filename_helper("input.out")
+    self.filename_helper("input")
 
   def test_main(self):
-    self.filename_helper("Main.out")
+    self.filename_helper("Main")
 
 if __name__ == '__main__':
   unittest.main()
