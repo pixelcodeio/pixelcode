7346ac4dcc9a0966eeaaf3bf53622cddf0fb094c
Young Kim <young-k@protonmail.com>
2017-11-19 18:00:46 -0500

    converted to python3

--- app/src/main.py
+++ app/src/main.py
@@ -28,14 +28,14 @@ def update_test_dir(path):
   """
   Generates ".out" files for any files in "./tests"
   """
-  print "Directory: " + path
+  print("Directory: " + path)
   files = os.listdir(path)
   svg = []
   for f in files:
     if ".svg" in f and f[0] != ".": # ignore temp files
       svg.append(f.split(".svg")[0])
   for f in svg:
-    print "Generating from file: " + f + ".svg"
+    print("Generating from file: " + f + ".svg")
     m = Main(path, f)
     o = open(path + f + ".out", "w+")
     o.write(m.convert_artboard())
