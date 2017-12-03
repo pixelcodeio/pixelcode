f25d3415a8769fb3337dcaaa7320637451401690
Kevin Chan <kevinchan159@gmail.com>
2017-12-03 18:13:52 -0500

    setup parser for navbars

--- app/src/layers/navbar.py
+++ app/src/layers/navbar.py
@@ -7,35 +7,32 @@ class NavBar(BaseLayer):
   Class representing a Navigation Bar in Sketch
   """
   def parse_elem(self, elem):
-    left_bar_button = None
-    right_bar_button = None
-    title = None
+    left_bar_buttons = []
+    right_bar_buttons = []
+    title_view = None
     for child in elem["children"]:
-      if child["type"] == "UIView":
-        if "barButton" in child["id"] or "BarButton" in child["id"]:
-          if child["x"] < 375.0/2:
-            if left_bar_button:
-              raise Exception("Navbar: Only one left button allowed.")
-            else:
-              left_bar_button = child
-          else:
-            if right_bar_button:
-              raise Exception("Navbar: Only one right button allowed.")
-            else:
-              right_bar_button = child
+      if "titleView" in child["id"] or "TitleView" in child["id"]:
+        if title_view:
+          raise Exception("Navbar: Only one title view allowed.")
         else:
-          if title:
-            raise Exception("Navbar: Only one title view allowed.")
-          else:
-            title = child
+          title_view = child
+      elif child["type"] == "UIButton":
+        if child["x"] < 375.0/2: # on left side of screen
+          left_bar_buttons.append(child)
+        else:
+          right_bar_buttons.append(child)
       else:
         raise Exception("Navbar: Navbar has unsupported elements.")
 
-    if title is None and left_bar_button is None and right_bar_button is None:
+    if title_view is None and not left_bar_buttons and not right_bar_buttons:
       raise Exception("Navbar: Navbar is empty.")
 
-    elem["left-bar-button"] = left_bar_button
-    elem["right-bar-button"] = right_bar_button
-    elem["title"] = title
+    # sort buttons by x value
+    left_bar_buttons = sorted(left_bar_buttons, key=lambda c: c.get('x'))
+    right_bar_buttons = sorted(right_bar_buttons, key=lambda c: c.get('x'))
+
+    elem["left-bar-buttons"] = left_bar_buttons
+    elem["right-bar-buttons"] = right_bar_buttons
+    elem["title-view"] = title_view
 
     return super().parse_elem(elem)
