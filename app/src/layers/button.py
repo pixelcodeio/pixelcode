906fb877ef05bd16049fd102d34223d7ee10c996
Kevin Chan <kevinchan159@gmail.com>
2017-12-04 13:00:16 -0500

    added background image to button.py

--- app/src/layers/button.py
+++ app/src/layers/button.py
@@ -9,16 +9,20 @@ class Button(BaseLayer):
   def parse_elem(self, elem):
     rect = None
     text = None
+    bg_img = None
     for child in elem["children"]:
       if child["type"] == "UIView":
         rect = child
       elif child["type"] == "UILabel":
         text = child
+      elif child["type"] == "UIImageView":
+        bg_img = child
 
-    if text is None:
-      raise Exception("Button: Text is empty in " + elem["id"])
+    if text is None and bg_img is None:
+      raise Exception("Button: Nothing in button in " + elem["id"])
 
     elem["rect"] = rect
     elem["text"] = text
+    elem["bg_img"] = bg_img
 
     return super().parse_elem(elem)
