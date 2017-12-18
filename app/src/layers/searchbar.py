d38f7831e1da911ba8f96e6365448986c4ca245b
Kevin Chan <kevinchan159@gmail.com>
2017-12-18 12:51:03 -0500

    added search/bookmark buttons for searchbar

--- app/src/layers/searchbar.py
+++ app/src/layers/searchbar.py
@@ -6,10 +6,26 @@ class SearchBar(BaseLayer):
   """
   def parse_elem(self, elem):
     contents = "Search" # default placeholder text to "Search"
+    search_button = None
+    bookmark_button = None
 
     for child in elem["children"]:
       if child["type"] == "UILabel":
         contents = child["textspan"][0]["contents"].decode('utf-8')
+      elif child["type"] == "UIButton":
+        center_x = elem['x'] + elem['rwidth']/2.0
+        if child['x'] < center_x:
+          if search_button:
+            raise Exception("Searchbar: Only one search button allowed.")
+          else:
+            search_button = child
+        else:
+          if bookmark_button:
+            raise Exception("Searchbar: Only one bookmark button allowed.")
+          else:
+            bookmark_button = child
 
     elem["contents"] = contents
+    elem["search-button"] = search_button
+    elem["bookmark-button"] = bookmark_button
     return super().parse_elem(elem)
