73d1bbdb2fe667872fec4e3d3d7765a381da6dac
Kevin Chan <kevinchan159@gmail.com>
2017-12-19 10:32:31 -0500

    fixed specification

--- app/src/components/uisearchbar.py
+++ app/src/components/uisearchbar.py
@@ -9,7 +9,7 @@ class UISearchBar(BaseComponent):
 
   def setup_component(self):
     """
-    Returns: swift code to setup uiview.
+    Returns: swift code to setup UISearchBar.
     """
     C = ('{0}.searchBarStyle = .minimal\n{0}.placeholder = "{1}"\n'
         ).format(self.id, self.info["contents"].decode('utf-8'))
