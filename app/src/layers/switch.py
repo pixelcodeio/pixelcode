291bd50d0890bab49dfaa015af0a6e1659c167e6
Kevin Chan <kevinchan159@gmail.com>
2017-12-20 12:42:37 -0500

    init switch.py

---
+++ app/src/layers/switch.py
@@ -0,0 +1,18 @@
+from . import *
+
+class Switch(BaseLayer):
+  """
+  Class representing a Switch in Sketch
+  """
+  def parse_elem(self, elem):
+    progress_fill = None
+
+    for child in elem["children"]:
+      if utils.word_in_str("progress", child["id"]):
+        if progress_fill:
+          raise Exception("Slider: Only one progress bar allowed.")
+        else:
+          progress_fill = child["fill"]
+
+    elem["progress_fill"] = progress_fill
+    return super().parse_elem(elem)
