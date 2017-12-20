76b873db15ffbf8e1249c607a2706127c6fd4e05
Kevin Chan <kevinchan159@gmail.com>
2017-12-20 12:45:19 -0500

    init uiswitch.py; added uiswitch to all

--- app/src/components/_all.py
+++ app/src/components/_all.py
@@ -4,6 +4,7 @@ from components.uilabel import UILabel
 from components.uinavbar import UINavBar
 from components.uisearchbar import UISearchBar
 from components.uislider import UISlider
+from components.uiswitch import UISwitch
 from components.uitabbar import UITabBar
 from components.uitablecollectionview import UITableCollectionView
 from components.uitextfieldview import UITextFieldView
