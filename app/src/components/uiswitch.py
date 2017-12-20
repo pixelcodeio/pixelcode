979be6e6ec1a211bbb8550bf7eac6882bf03502b
Kevin Chan <kevinchan159@gmail.com>
2017-12-20 12:49:58 -0500

    finished switch

--- app/src/components/uiswitch.py
+++ app/src/components/uiswitch.py
@@ -11,10 +11,4 @@ class UISwitch(BaseComponent):
     """
     Returns: swift code to setup UISlider.
     """
-    C = ("{}.isContinuous = true\n").format(self.id)
-
-    if self.info.get('progress_fill'):
-      C += ("{}.minimumTrackTintColor = {}\n"
-           ).format(self.id, utils.create_uicolor(self.info['progress_fill']))
-
-    return C
+    return ""
