f3d7c7ec29f4f6dcf9aefa088ac5660facaad068
Kevin Chan <kevinchan159@gmail.com>
2018-01-10 11:14:42 -0500

    removed children from text.py

--- app/src/pixelcode/plugin/layers/text.py
+++ app/src/pixelcode/plugin/layers/text.py
@@ -27,4 +27,5 @@ class Text(BaseLayer):
       elem["line-spacing"] = elem["line-spacing"]
     if "letter-spacing" in elem.attrs:
       elem["char-spacing"] = elem["letter-spacing"]
+    elem["children"] = None
     return super().parse_elem(elem)
